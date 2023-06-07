67b3b299d3d6202f372c1a410c3af49878b16d59
MendozaJulioC <juliomendoza.medellin@gmail.com>
2023-06-07 12:33:14 -0500

    cambio ruta puerto server

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(require('./routes/j4.routes'))
 
 
 async function main(){
-    app.listen(process.env.PORT||3001);
+    app.listen(process.env.AWS_PORT||3001);
     console.log(`Service on port ${process.env.AWS_PORT}`);
 }
 
