eaabe48cd9bad279ca821b3b698b9681351c2b0a
MendozaJulioC <juliomendoza.medellin@gmail.com>
2023-06-07 04:17:53 -0500

    cambios conex

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const dblocal = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT
+    user: process.env.AWS_PGUSER,
+    host: process.env.AWS_PGHOST,
+    database: process.env.AWS_PGDATABASE,
+    password: process.env.AWS_PGPASSWORD,
+    port: process.env.AWS_PGPORT
 })
 dblocal.connect().then(()=>console.log('Conexión  DB'));
 
